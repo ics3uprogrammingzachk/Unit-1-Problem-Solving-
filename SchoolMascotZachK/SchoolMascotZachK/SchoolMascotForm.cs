15cea16f902c9c186e5f8e0148bfee2b357e0061
ZacharyKr <32547158+ZacharyKr@users.noreply.github.com>
2019-02-12 12:17:47 -0500

    Initial commit

---
+++ SchoolMascotZachK/SchoolMascotZachK/SchoolMascotForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolMascotZachK
+{
+    public partial class frmSchoolMascot : Form
+    {
+        public frmSchoolMascot()
+        {
+            InitializeComponent();
+        }
+
+        private void SchoolMascotForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

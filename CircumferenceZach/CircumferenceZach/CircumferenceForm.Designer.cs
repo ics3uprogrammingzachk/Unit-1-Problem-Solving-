6d6e336fbdf300eab3536c8fd6fb11670a66396b
ZacharyKr <32547158+ZacharyKr@users.noreply.github.com>
2019-02-20 13:11:22 -0500

    added circmference

--- CircumferenceZach/CircumferenceZach/CircumferenceForm.Designer.cs
+++ CircumferenceZach/CircumferenceZach/CircumferenceForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CircumferenceZach
 {
-    partial class Form1
+    partial class frmCircumference
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRadius.Location = new System.Drawing.Point(12, 37);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(234, 17);
+            this.lblRadius.TabIndex = 0;
+            this.lblRadius.Text = "Enter the radius of the circle (in cm)";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(252, 36);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 1;
+            this.txtRadius.TextChanged += new System.EventHandler(this.txtRadius_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(147, 77);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(99, 37);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(12, 188);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(21, 23);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "?";
+            this.lblAnswer.Visible = false;
+            // 
+            // frmCircumference
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(388, 262);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblRadius);
+            this.Name = "frmCircumference";
+            this.Text = "Circumference";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswer;
     }
 }
 
--- CircumferenceZach/CircumferenceZach/CircumferenceForm.cs
+++ CircumferenceZach/CircumferenceZach/CircumferenceForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Zachary Kranabetter
+ * Created on: Febuary 20th 2019 (Connor's Birthday)
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #11 - Top-Down Design & Flow Charts
+ * This program calculates the circumference of a circle with a given radius
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +18,36 @@ using System.Windows.Forms;
 
 namespace CircumferenceZach
 {
-    public partial class Form1 : Form
+    public partial class frmCircumference : Form
     {
-        public Form1()
+        public frmCircumference()
         {
             InitializeComponent();
         }
+
+        const double PI = Math.PI;
+
+        private void txtRadius_TextChanged(object sender, EventArgs e)
+        {
+            lblAnswer.Visible = false;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double radius = Convert.ToDouble(txtRadius.Text); 
+
+            double circumference = PI * radius * 2;
+
+            lblAnswer.Text = Convert.ToString("The circumference is" + circumference);
+
+            lblAnswer.Visible = true;
+        }
+
+        private void lblAnswer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
